5df45b5392a74787f2cd82f64aa326c536cec6f7
Sujay Naik <sujaynaik142@gmail.com>
2026-07-24 01:29:27 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { useUserStore } from '@/stores/user'
 import Home from '@/views/Home.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const Checkout = () => import('@/views/Checkout.vue')
 const Cart = () => import('@/views/Cart.vue')
@@ -9,7 +9,7 @@ const Product = () => import('@/views/Product.vue')
 const Register = () => import('@/views/Register.vue')
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
